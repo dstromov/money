e0edaca3c1b41c19a4d79ec1fb6bc8a6a1c3163c
dstromov <vlz.lpk@gmail.com>
2025-07-16 18:38:49 +0300

    Update Composition.php

--- app/Models/Composition.php
+++ app/Models/Composition.php
@@ -6,14 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+
+//выписывать поля в явном виде, аннотация, как проперти, выше класса, прямо тут.
+// может вернуться коллекция, нужно использовать "<>"
+
+
+
 class Composition extends Model
 {
+
+    // в явном виде прописывать с чем ассоциируется таблица.
+
+    
     use SoftDeletes;
 
+
+    // лучше не разрешать обновление вообще всех полей, т.к. так можно id обновить.
+    // лучше использовать fillable и перечислить в явном виде поля.
     protected $guarded = [];
     
     public function denomination(): BelongsTo
     {
+        // правильно, но нужно добавить ключи - в явном виде ключи.
         return $this->belongsTo(Denomination::class);
     }
 
